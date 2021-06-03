76900e5bf54dfdb2f264e03f6dc405bc487bda11
SIBI M. S <78226316+SIBI-MS@users.noreply.github.com>
2021-06-04 00:44:45 +0530

    Delete 1ST-SEM-Q directory

--- 1ST-SEM-Q/interchange2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	float a,b;
-	printf("ENTER TWO NUMBERS : ");
-	scanf("%f%f",&a,&b);
-	printf("a=%f\nb=%f\n\n",a,b);
-	printf("b=%f\na=%f\b",a,b);
-}
--- 1ST-SEM-Q/leapyearconditional.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-  int y;
-  printf("Enter the year:");
-  scanf("%d",&y);
-  y=((y%100==0 && y%400==0) || (y%100!=0 && y%4==0))?printf("Leap year-%d\n",y):printf("Not leap year\n");
-}
