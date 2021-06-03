af81d030c9db1d53a7da3c6eb61f9d06f9c9a9c3
SIBI M. S <78226316+SIBI-MS@users.noreply.github.com>
2021-06-04 00:36:25 +0530

    Add files via upload

---
+++ SEM-1/CREATION/sidepyramidbyforloop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+ {
+   int i, j, r;
+   printf("Enter the number of rows: ");
+   scanf("%d", &r);
+   for (i = 1; i <= r; ++i) 
+   {
+     for (j = 1; j <= i; ++j) {
+     printf("* ");
+   }
+      printf("\n");
+   }
+}
