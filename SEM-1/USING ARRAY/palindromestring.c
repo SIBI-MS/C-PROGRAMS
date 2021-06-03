c6a672806e5d4db03a01d889357d37adf1c472d9
SIBI M. S <78226316+SIBI-MS@users.noreply.github.com>
2021-06-04 00:35:31 +0530

    Add files via upload

---
+++ SEM-1/USING ARRAY/palindromestring.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+  char a[20];
+  int i,length=0,j;
+  printf("enter the word:");
+  scanf("%s",a);
+  for(i=0;a[i]!='\0';i++)
+  {
+    length++;
+    printf("%d",length);
+  }
+  for(i=0,j=length-1;i<j;i++,j--)
+  {
+    printf("%d\n",i);
+    if(a[i]!=a[j])
+    {
+      break;
+    }
+  }
+  if(i<j)
+  {
+    printf("not palindrome\n");
+  }
+  else
+  {
+    printf("palindrome\n");
+  }
+}
